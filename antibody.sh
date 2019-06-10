78391f5f36293047288906deb51a0c334e32180b
Angad Singh <mail@angad.dev>
2019-06-10 05:44:58 +0530

    Changes

--- antibody.sh
+++ antibody.sh
@@ -7,6 +7,7 @@ antibody bundle arzzen/calc.plugin.zsh
 antibody bundle chrissicool/zsh-256color
 antibody bundle djui/alias-tips
 antibody bundle zsh-users/zsh-completions
-antibody bundle denysdovhan/spaceship-prompt
+# antibody bundle denysdovhan/spaceship-prompt
+
 # kind:path
 antibody bundle caarlos0/ports kind:path
