5ad70acd2e199eed314a1c8761baef9724346c96
Angad Singh <mail@angad.dev>
2019-07-10 00:17:08 +0530

    Add Git aliases

--- aliases.sh
+++ aliases.sh
@@ -1,11 +1,22 @@
-alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
-alias g='hub'
+alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
 alias la="ls -la"
 alias rc="source $HOME/.zshrc"
-alias td=todolist
-alias startApache='/Applications/MAMP/bin/startApache.sh'
-alias stopApache='/Applications/MAMP/bin/stopApache.sh'
-alias startMysql='/Applications/MAMP/bin/startMysql.sh'
-alias stopMysql='/Applications/MAMP/bin/stopMysql.sh'
-alias gohome='cd /Users/dotangad/code/go/src/github.com/dotangad/'
-alias ip='dig +short myip.opendns.com @resolver1.opendns.com'
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+
+alias startApache="/Applications/MAMP/bin/startApache.sh"
+alias stopApache="/Applications/MAMP/bin/stopApache.sh"
+alias startMysql="/Applications/MAMP/bin/startMysql.sh"
+alias stopMysql="/Applications/MAMP/bin/stopMysql.sh"
+
+alias gohome="cd /Users/dotangad/code/go/src/github.com/dotangad/"
+
+alias g="hub"
+alias gaa="git add --all"
+alias gst="git status"
+alias gp="git push"
+func gcmsg () {
+	git commit -m $1;
+}
+func gpu () {
+	git push -u $1 $2;
+}
