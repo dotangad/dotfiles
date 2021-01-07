46241ffc3ce716e0664c1cdb46063581d59c3ac5
Angad Singh <mail@angad.dev>
2021-01-07 16:28:24 +0530

    Updtes

--- fish/config.fish
+++ fish/config.fish
@@ -17,6 +17,7 @@ set PATH $HOME/bin $PATH
 set PATH $HOME/bin/node $PATH
 set PATH $HOME/.yarn/bin $PATH
 set PATH $HOME/.config/yarn/global/node_modules/.bin $PATH
+set PATH $HOME/src/flutter/bin $PATH
 
 function fish_user_key_bindings
   fish_vi_key_bindings
