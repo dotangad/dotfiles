a68af46ebc560e8b0211cf46c55ec83914c47782
Angad Singh <mail@angad.dev>
2022-09-22 20:43:13 +0530

    nvim: fresh start

--- nvim/.config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
--- vim.cmd("scriptencoding utf-8")
-vim.cmd("autocmd!")
-
-require("my.options")
-require("my.keymap")
-require("my.plugins")
--- nvim/.config/nvim/lua/my/markdown.lua
+++
@@ -1,8 +0,0 @@
-vim.g.vim_markdown_folding_disabled = 1
-vim.cmd([[
-  let g:vim_markdown_folding_disabled = 1
-  autocmd FileType markdown set wrap
-  autocmd FileType markdown set linebreak
-  autocmd FileType markdown setlocal spell spelllang=en_us
-  autocmd FileType markdown vmap <leader><Bslash> :EasyAlign*<Bar><Enter>
-]])
