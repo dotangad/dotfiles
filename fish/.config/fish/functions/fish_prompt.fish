508f08b36747499d861e8a272fe5a5a0894d0f24
Angad Singh <mail@angad.dev>
2022-09-26 03:17:57 +0530

    (fish) git prompt tuning

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -10,7 +10,7 @@ function fish_prompt --description 'Write out the prompt'
     set -g __fish_git_prompt_char_upstream_prefix " "
     
     set -g __fish_git_prompt_char_stagedstate "●"
-    set -g __fish_git_prompt_char_dirtystate "✚"
+    set -g __fish_git_prompt_char_dirtystate "+"
     set -g __fish_git_prompt_char_untrackedfiles "..."
     set -g __fish_git_prompt_char_conflictedstate "✖"
     set -g __fish_git_prompt_char_cleanstate "✔"
