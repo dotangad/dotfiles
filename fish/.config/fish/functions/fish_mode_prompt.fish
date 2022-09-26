d56d568c3fdd7bae6f8998d3f5889c9e977132c1
Angad Singh <mail@angad.dev>
2022-09-26 09:18:06 +0530

    (fish) messing around with mode fish_mode_prompt

--- fish/.config/fish/functions/fish_mode_prompt.fish
+++ fish/.config/fish/functions/fish_mode_prompt.fish
@@ -5,16 +5,16 @@ function fish_mode_prompt
   switch $fish_bind_mode
     case default
       set vi_mode_color (set_color "#ff0000" --bold)
-      set vi_mode_symbol "[N]"
+      set vi_mode_symbol "[λ]"
     case insert
       set vi_mode_color (set_color "#00ff00" --bold)
       set vi_mode_symbol "[λ]"
     case replace replace_one
       set vi_mode_color (set_color "#0000ff" --bold)
-      set vi_mode_symbol "[R]"
+      set vi_mode_symbol "[λR"
     case visual
       set vi_mode_color (set_color "#0000ff" --bold)
-      set vi_mode_symbol "[V]"
+      set vi_mode_symbol "[λV"
   end
 
   # string unescape "$vi_mode_color $vi_mode_symbol \x1b[0m "
